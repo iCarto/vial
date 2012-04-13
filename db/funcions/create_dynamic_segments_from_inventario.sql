e381f903e59bfe6cccef33442f598104b23856eb
Andrés Maneiro <amaneiro@icarto.es>
2012-04-13 20:49:36 +0200

    Allow the segments to be composed by multilinestrings

--- db/funcions/create_dynamic_segments_from_inventario.sql
+++ db/funcions/create_dynamic_segments_from_inventario.sql
@@ -1,12 +1,10 @@
 
-SELECT AddGeometryColumn('inventario', 'ancho_plataforma', 'the_geom', '23029', 'LINESTRING', 2);
-ALTER TABLE inventario.ancho_plataforma DROP CONSTRAINT enforce_geotype_the_geom;
-ALTER TABLE inventario.ancho_plataforma DROP CONSTRAINT enforce_dims_the_geom;
+SELECT AddGeometryColumn('inventario', 'ancho_plataforma', 'the_geom', '23029', 'MULTILINESTRINGM', 3);
 
 UPDATE inventario.ancho_plataforma AS a2 SET the_geom = (
-       SELECT (ST_Dump(ST_Locate_Between_Measures(c.the_geom,
+       SELECT ST_CollectionExtract(ST_Locate_Between_Measures(c.the_geom,
                                 a.origentram,
-                                a.finaltramo))).geom
+                                a.finaltramo), 2)
        FROM inventario.carreteras AS c, inventario.ancho_plataforma AS a
        WHERE c.codigo = a.numeroinve AND a.gid = a2.gid)
 ;
