4e0da3edcbcf736acb482a4dd8823100a74a4256
Andrés Maneiro <amaneiro@icarto.es>
2012-06-21 19:09:14 +0200

    Some usability improvements: pk enabling/disabling & focus policy

--- src/es/icarto/gvsig/viasobras/catalog/view/CatalogForm.java
+++ src/es/icarto/gvsig/viasobras/catalog/view/CatalogForm.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.sql.SQLException;
 import java.util.HashMap;
 
@@ -14,6 +16,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.LayoutFocusTraversalPolicy;
 import javax.swing.table.TableModel;
 
 import com.iver.andami.messages.NotificationManager;
@@ -35,48 +38,82 @@ public class CatalogForm extends JPanel implements IWindow, SingletonWindow {
 
     private FormPanel form;
     protected WindowInfo viewInfo = null;
+
     private HashMap<String, JComponent> widgets;
     private HashMap<String, JButton> buttons;
-    private ItemListener carreteraUpdater;
-    private ItemListener concelloUpdater;
+
     private JComboBox carreteras;
+    private ItemListener carreteraUpdater;
     private JComboBox concellos;
-    private TableModel tipoPavimento;
-    private TableModel anchoPlataforma;
+    private ItemListener concelloUpdater;
 
     private JTextField pkStart;
     private JTextField pkEnd;
 
+    private JTable tipoPavimento;
+    private TableModel tipoPavimentoModel;
+    private JTable anchoPlataforma;
+    private TableModel anchoPlataformaModel;
+
+    private JButton search;
+    private JButton load;
+    private JButton save;
+
     public CatalogForm() {
 	form = new FormPanel("inventarioform.xml");
-	initForm();
-	initListeners();
 	Catalog.clear();
+	initForm();
 	JScrollPane scrolledForm = new JScrollPane(form);
 	this.add(scrolledForm);
     }
 
     private void initForm() {
+
 	widgets = AbeilleParser.getWidgetsFromContainer(form);
 	buttons = AbeilleParser.getButtonsFromContainer(form);
+
 	carreteras = (JComboBox) widgets.get("carretera");
 	concellos = (JComboBox) widgets.get("concello");
+
 	pkStart = (JTextField) widgets.get("pk_inicial");
 	pkEnd = (JTextField) widgets.get("pk_final");
+	pkStart.setEnabled(false);
+	pkEnd.setEnabled(false);
+
+	tipoPavimento = (JTable) widgets.get("tabla_tipo_pavimento");
+	anchoPlataforma = (JTable) widgets.get("tabla_ancho_plataforma");
+
+	search = (JButton) buttons.get("buscar");
+	load = (JButton) buttons.get("cargar");
+	save = (JButton) buttons.get("guardar");
+
 	fillComboBoxes();
-	connectButtonsToActions();
+	fillTables();
+
+	initFocus();
+	initListeners();
+    }
+
+    private void initFocus() {
+	this.setFocusCycleRoot(true);
+	this.setFocusTraversalPolicy(new LayoutFocusTraversalPolicy());
+	carreteras.requestFocusInWindow();
+	tipoPavimento.setFocusable(false);
+	anchoPlataforma.setFocusable(false);
     }
 
     private void initListeners() {
-	carreteraUpdater = new ItemListener() {
 
+	carreteraUpdater = new ItemListener() {
 	    public void itemStateChanged(ItemEvent e) {
 		if (e.getStateChange() == ItemEvent.SELECTED) {
 		    if (carreteras.getSelectedItem().equals(VOID_ITEM)) {
 			Catalog.setCarretera(Catalog.CARRETERA_NONE);
+			disablePKControls();
 		    } else {
 			Carretera c = (Carretera) carreteras.getSelectedItem();
 			Catalog.setCarretera(c.getCode());
+			enablePKControls();
 		    }
 		    fillConcellos();
 		}
@@ -84,14 +121,19 @@ public class CatalogForm extends JPanel implements IWindow, SingletonWindow {
 	};
 
 	concelloUpdater = new ItemListener() {
-
 	    public void itemStateChanged(ItemEvent e) {
 		if (e.getStateChange() == ItemEvent.SELECTED) {
 		    if (concellos.getSelectedItem().equals(VOID_ITEM)) {
 			Catalog.setConcello(Catalog.CONCELLO_NONE);
+			if (Catalog.getCarreteraSelected() != Catalog.CARRETERA_NONE) {
+			    enablePKControls();
+			} else {
+			    disablePKControls();
+			}
 		    } else {
 			Concello c = (Concello) concellos.getSelectedItem();
 			Catalog.setConcello(c.getCode());
+			disablePKControls();
 		    }
 		}
 	    }
@@ -100,28 +142,30 @@ public class CatalogForm extends JPanel implements IWindow, SingletonWindow {
 
 	carreteras.addItemListener(carreteraUpdater);
 	concellos.addItemListener(concelloUpdater);
-    }
 
-    private void connectButtonsToActions() {
-	JButton search = (JButton) buttons.get("buscar");
+	KeyListener mySearchListener = new KeyListener() {
+	    public void keyPressed(KeyEvent arg0) {
+	    }
+
+	    public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+		    setPKOnCatalog();
+		    fillTables();
+		}
+	    }
+
+	    public void keyTyped(KeyEvent arg0) {
+	    }
+	};
+	pkStart.addKeyListener(mySearchListener);
+	pkEnd.addKeyListener(mySearchListener);
+
 	search.addActionListener(new ActionListener() {
 	    public void actionPerformed(ActionEvent arg0) {
-		if (!pkStart.getText().equals("")) {
-		    double d = Double.parseDouble(pkStart.getText());
-		    Catalog.setPKStart(d);
-		} else {
-		    Catalog.setPKStart(Catalog.PK_NONE);
-		}
-		if (!pkEnd.getText().equals("")) {
-		    double d = Double.parseDouble(pkEnd.getText());
-		    Catalog.setPKEnd(d);
-		} else {
-		    Catalog.setPKEnd(Catalog.PK_NONE);
-		}
+		setPKOnCatalog();
 		fillTables();
 	    }
 	});
-	JButton load = (JButton) buttons.get("cargar");
 	load.addActionListener(new ActionListener() {
 
 	    public void actionPerformed(ActionEvent arg0) {
@@ -129,29 +173,52 @@ public class CatalogForm extends JPanel implements IWindow, SingletonWindow {
 	    }
 
 	});
-	JButton save = (JButton) buttons.get("guardar");
 	save.addActionListener(new ActionListener() {
 	    public void actionPerformed(ActionEvent e) {
 		try {
-		    ((TramosTableModel) tipoPavimento).saveChanges();
-		    ((TramosTableModel) anchoPlataforma).saveChanges();
+		    ((TramosTableModel) tipoPavimentoModel).saveChanges();
+		    ((TramosTableModel) anchoPlataformaModel).saveChanges();
 		} catch (SQLException e1) {
 		    NotificationManager.addError(e1);
 		}
 	    }
 	});
+
+    }
+
+    private void enablePKControls() {
+	pkStart.setEnabled(true);
+	pkEnd.setEnabled(true);
+    }
+
+    private void disablePKControls() {
+	pkStart.setEnabled(false);
+	pkEnd.setEnabled(false);
+    }
+
+    private void setPKOnCatalog() {
+	if (!pkStart.getText().equals("")) {
+	    double d = Double.parseDouble(pkStart.getText());
+	    Catalog.setPKStart(d);
+	} else {
+	    Catalog.setPKStart(Catalog.PK_NONE);
+	}
+	if (!pkEnd.getText().equals("")) {
+	    double d = Double.parseDouble(pkEnd.getText());
+	    Catalog.setPKEnd(d);
+	} else {
+	    Catalog.setPKEnd(Catalog.PK_NONE);
+	}
     }
 
     private void fillTables() {
-	JTable tbTipoPavimento = (JTable) widgets.get("tabla_tipo_pavimento");
-	JTable tbAnchoPlataforma = (JTable) widgets
-		.get("tabla_ancho_plataforma");
 	try {
-	    tipoPavimento = Catalog.getTramosTipoPavimento().getTableModel();
-	    tbTipoPavimento.setModel(tipoPavimento);
-	    anchoPlataforma = Catalog.getTramosAnchoPlataforma()
+	    tipoPavimentoModel = Catalog.getTramosTipoPavimento()
 		    .getTableModel();
-	    tbAnchoPlataforma.setModel(anchoPlataforma);
+	    tipoPavimento.setModel(tipoPavimentoModel);
+	    anchoPlataformaModel = Catalog.getTramosAnchoPlataforma()
+		    .getTableModel();
+	    anchoPlataforma.setModel(anchoPlataformaModel);
 	    this.repaint();
 	} catch (SQLException e) {
 	    NotificationManager.addError(e);
