a377d2f0db659485eacd585dcc8636a57d0151e5
Andrés Maneiro <andres.maneiro@gmail.com>
2012-08-11 15:33:59 +0200

    Adding test suite

---
+++ src-test/es/icarto/gvsig/viasobras/domain/tests/AllTests.java
@@ -0,0 +1,11 @@
+package es.icarto.gvsig.viasobras.domain.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ CatalogEditTests.class, CatalogSearchTests.class })
+public class AllTests {
+}
