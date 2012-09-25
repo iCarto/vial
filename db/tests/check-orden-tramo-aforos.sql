eacbfc494d4a27e63f7599d3da3c35a0812f6b24
Andrés Maneiro <amaneiro@icarto.es>
2012-09-25 17:26:53 +0200

    Testing aforos - update to new names in columns

--- db/tests/check-orden-tramo-aforos.sql
+++ db/tests/check-orden-tramo-aforos.sql
@@ -1,6 +1,6 @@
 SELECT plan(COUNT(*)::int) FROM inventario.aforos;
 
-SELECT ok(orden IS NOT NULL, 'orden is NOT NULL, gid = '||gid)
+SELECT ok(tramo IS NOT NULL, 'tramo is NOT NULL, gid = '||gid)
     FROM inventario.aforos;
 
 SELECT * FROM finish();
