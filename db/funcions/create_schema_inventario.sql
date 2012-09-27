40eb9cc40a8146c95d775138081d06a8541b423e
Andrés Maneiro <amaneiro@icarto.es>
2012-09-27 21:36:10 +0200

    Added info_base in ETRS89

--- db/funcions/create_schema_inventario.sql
+++ db/funcions/create_schema_inventario.sql
@@ -1,2 +1,3 @@
 DROP SCHEMA IF EXISTS inventario CASCADE;
+DELETE FROM public.geometry_columns WHERE f_table_schema = 'inventario';
 CREATE SCHEMA inventario;
--- db/funcions/drop_schema_infobase.sql
+++ db/funcions/drop_schema_infobase.sql
@@ -1,1 +1,2 @@
 DROP SCHEMA IF EXISTS info_base CASCADE;
+DELETE FROM public.geometry_columns WHERE f_table_schema = 'info_base';
