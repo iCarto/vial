239a664867ed5c06a50274668f777ca9bf85b315
Andrés Maneiro <amaneiro@icarto.es>
2012-10-29 17:30:40 +0100

    C03 & C04

---
+++ db/funcions/consultas/c04-tramos-antiguos-general.sql
@@ -0,0 +1,27 @@
+SELECT m.nombre AS "Municipio", \
+       i.codigo_carretera AS "Código (LU-P)", \
+       i.tramo AS "Tramo", \
+       c.denominacion AS "Denominación", \
+       i.orden_variante AS "Código", \
+       i.orden_variante_tramo AS "Orden", \
+       i.origen_via_pk AS "PK origen vía", \
+       i.final_via_pk AS "PK final vía", \
+       i.longitud AS "Longitud", \
+       i.estado AS "Estado", \
+       i.ancho_plataforma AS "Ancho plataforma", \
+       i.tipo_pavimento AS "Tipo pavimento", \
+       i.observaciones AS "Observaciones" \
+FROM inventario.variantes i, \
+     inventario.carreteras c, \
+     inventario.municipio_codigo m \
+WHERE i.codigo_carretera = c.numero \
+      AND i.codigo_municipio = m.codigo \
+      [[WHERE]] \
+ORDER BY m.nombre, \
+         i.codigo_carretera, \
+         i.tramo, \
+         i.orden_variante, \
+         i.orden_variante_tramo, \
+         i.pk_inicial, \
+         i.pk_final \
+;
