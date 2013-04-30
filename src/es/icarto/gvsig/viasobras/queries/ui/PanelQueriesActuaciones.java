efc068873d507a7a59d13355c61296c1bfff4ffb
psanxiao <psanxiao@icarto.es>
2013-04-30 09:53:22 +0200

    Refs #367: Added UI for actuaciones queries

--- src/es/icarto/gvsig/viasobras/queries/ui/PanelQueriesActuaciones.java
+++ src/es/icarto/gvsig/viasobras/queries/ui/PanelQueriesActuaciones.java
@@ -5,12 +5,13 @@ import javax.swing.JScrollPane;
 import com.jeta.forms.components.panel.FormPanel;
 
 
+@SuppressWarnings("serial")
 public class PanelQueriesActuaciones extends gvWindow {
 
     private FormPanel form;
 
     public PanelQueriesActuaciones(boolean b) {
-	super(600, 400, true);
+	super(460, 620, true);
 	form = new FormPanel("consultas-actuaciones-ui.xml");
 	JScrollPane scrolledForm = new JScrollPane(form);
 	this.add(scrolledForm);
