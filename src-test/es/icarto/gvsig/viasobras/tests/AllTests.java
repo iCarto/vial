af2499f170d7ce42359f7d709e3b39033288f805
Andrés Maneiro <amaneiro@icarto.es>
2013-04-30 18:22:27 +0200

    Refs #367 - Added more tests

--- src-test/es/icarto/gvsig/viasobras/tests/AllTests.java
+++ src-test/es/icarto/gvsig/viasobras/tests/AllTests.java
@@ -6,10 +6,11 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import es.icarto.gvsig.viasobras.tests.catalog.AllTestsCatalog;
 import es.icarto.gvsig.viasobras.tests.forms.AllTestsForm;
-import es.icarto.gvsig.viasobras.tests.queries.TestsQueriesEstado;
+import es.icarto.gvsig.viasobras.tests.queries.AllTestsQueries;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AllTestsCatalog.class, AllTestsForm.class, TestsQueriesEstado.class })
+@SuiteClasses({ AllTestsCatalog.class, AllTestsForm.class,
+	AllTestsQueries.class })
 public class AllTests {
 
 }
---
+++ src-test/es/icarto/gvsig/viasobras/tests/queries/AllTestsQueries.java
@@ -0,0 +1,11 @@
+package es.icarto.gvsig.viasobras.tests.queries;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestsQueriesActuaciones.class, TestsQueriesEstado.class })
+public class AllTestsQueries {
+}
