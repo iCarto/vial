6271d6d193f8f6a5cc0d6eec7177e2b747828d2e
psanxiao <psanxiao@icarto.es>
2013-05-08 09:37:28 +0200

    Fixed how permisions are granted to read only user since database version is 8.4

--- db/viasobras-postprocessing.sh
+++ db/viasobras-postprocessing.sh
@@ -7,10 +7,14 @@ config_file=$1
 
 # Setting permisions to read only user
 SCHEMAS=(consultas elle info_base inventario public)
+TABLES=(consultas.aforos consultas.consultas consultas.consultas_actuaciones elle._map elle._map_overview elle._map_overview_style elle._map_style info_base.embalse info_base.municipios info_base.municipios_lugo info_base.oceano info_base.portugal info_base.provincias_galicia info_base.provincias_limitrofes info_base.red_carreteras info_base.rio_a info_base.rio_l inventario.accidentes inventario.actuacion_municipio inventario.actuaciones inventario.aforos inventario.ancho_plataforma inventario.carretera_municipio inventario.carreteras inventario.carreteras_lugo inventario.cotas inventario.municipio_codigo inventario.pks_1000 inventario.rampas inventario.tipo_pavimento inventario.variantes public.geometry_columns public.spatial_ref_sys)
+
 for i in "${SCHEMAS[@]}"; do
     psql -h $viasobras_server -U $viasobras_superuser -p $viasobras_port -c "grant usage on schema $i to $viasobras_user_lectura" $viasobras_dbname;
-    psql -h $viasobras_server -U $viasobras_superuser -p $viasobras_port -c "grant select on all tables in schema $i to $viasobras_user_lectura" $viasobras_dbname;
-    psql -h $viasobras_server -U $viasobras_superuser -p $viasobras_port -c "alter default privileges in schema $i grant select on tables to $viasobras_user_lectura" $viasobras_dbname;
+done
+
+for i in "${TABLES[@]}"; do
+    psql -h $viasobras_server -U $viasobras_superuser -p $viasobras_port -c "grant select on $i to $viasobras_user_lectura" $viasobras_dbname;
 done
 
 psql -h $viasobras_server -p $viasobras_port -U $viasobras_superuser \
