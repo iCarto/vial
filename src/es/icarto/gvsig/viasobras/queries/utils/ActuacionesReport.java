0cebfff304961b27108ae137c9725d16b2d4b264
psanxiao <psanxiao@icarto.es>
2013-05-08 16:58:45 +0200

    Refs #367: Fixed size of tipo field on queries

--- src/es/icarto/gvsig/viasobras/queries/utils/ActuacionesReport.java
+++ src/es/icarto/gvsig/viasobras/queries/utils/ActuacionesReport.java
@@ -8,6 +8,7 @@ public class ActuacionesReport extends Report {
     private final static float MUNICIPIO = 60f;
     private final static float CODIGO_AND_PK_FIELDS = 40f;
     private final static float CODIGO_ACTUACION = 50f;
+    private final static float TIPO = 100f;
     private final static float OBSERVACIONES = 120f;
 
     public ActuacionesReport(int reportType, String fileName,
@@ -26,12 +27,14 @@ public class ActuacionesReport extends Report {
 		columnsWidth[i] = CODIGO_AND_PK_FIELDS;
 	    }else if (i == 4) {
 		columnsWidth[i] = CODIGO_ACTUACION;
+	    }else if (i == 5) {
+		columnsWidth[i] = TIPO;
 	    }else if (i == columnCount-1) {
 		columnsWidth[i] = OBSERVACIONES;
 	    }else {
 		columnsWidth[i] = ((table.getTotalWidth()
-			-MUNICIPIO-(CODIGO_AND_PK_FIELDS*3)-CODIGO_ACTUACION-OBSERVACIONES)
-			/(columnCount-6));
+			-MUNICIPIO-(CODIGO_AND_PK_FIELDS*3)-CODIGO_ACTUACION-TIPO-OBSERVACIONES)
+			/(columnCount-7));
 	    }
 	}
 	return columnsWidth;
