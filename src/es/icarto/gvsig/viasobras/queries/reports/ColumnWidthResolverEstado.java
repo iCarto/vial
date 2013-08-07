174239eeba51665d8320d63c5696574a8ff72609
Andrés Maneiro <amaneiro@icarto.es>
2013-08-07 12:08:24 +0200

    PDF estado: adjust size by percent

--- src/es/icarto/gvsig/viasobras/queries/reports/ColumnWidthResolverEstado.java
+++ src/es/icarto/gvsig/viasobras/queries/reports/ColumnWidthResolverEstado.java
@@ -5,22 +5,38 @@ import com.lowagie.text.pdf.PdfPTable;
 
 public class ColumnWidthResolverEstado implements ColumnWidthResolver {
 
-    private static final float DENOMINACION_COLUMN_WIDTH = 200f;
-    private static final float MUNICIPIOS_COLUMN_WIDTH = 200f;
 
     public float[] getColumnsWidth(PdfPTable table, int columnCount) {
 	float[] columnsWidth = new float[columnCount];
+	float widthTotal        = table.getTotalWidth();
+
+	float widthCM           = widthTotal * 0.03f;
+	float widthMunicipio    = widthTotal * 0.08f;
+	float widthCodigoLUP    = widthTotal * 0.08f;
+	float widthTramo        = widthTotal * 0.04f;
+	float widthDenominacion = widthTotal * 0.24f;
+	// float widthObservaciones = widthTotal * 0.1f;
+
+	float widthAvailable = widthTotal - widthCM - widthMunicipio
+		- widthCodigoLUP - widthTramo - widthDenominacion;
+	float widthAverage = widthAvailable / (columnCount - 5);
+
 	for (int i = 0; i < columnCount; i++) {
-	    if (i == 3) {
-		columnsWidth[i] = DENOMINACION_COLUMN_WIDTH;
-	    } else if (i == 8) {
-		columnsWidth[i] = MUNICIPIOS_COLUMN_WIDTH;
+	    if (i == 0) {
+		columnsWidth[i] = widthCM;
+	    } else if (i == 1) {
+		columnsWidth[i] = widthMunicipio;
+	    } else if (i == 2) {
+		columnsWidth[i] = widthCodigoLUP;
+	    } else if (i == 3) {
+		columnsWidth[i] = widthTramo;
+	    } else if (i == 4) {
+		columnsWidth[i] = widthDenominacion;
 	    } else {
-		columnsWidth[i] = (table.getTotalWidth()
-			- DENOMINACION_COLUMN_WIDTH - MUNICIPIOS_COLUMN_WIDTH)
-			/ (columnCount - 2);
+		columnsWidth[i] = widthAverage;
 	    }
 	}
+
 	return columnsWidth;
     }
 
