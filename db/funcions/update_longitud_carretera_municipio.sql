41e9ab669bc34ac8ed817e9d695dbb04046b7d72
Andrés Maneiro <amaneiro@icarto.es>
2015-04-20 18:41:49 +0200

    force cast to integer

--- db/funcions/update_longitud_carretera_municipio.sql
+++ db/funcions/update_longitud_carretera_municipio.sql
@@ -5,11 +5,11 @@ CREATE OR REPLACE FUNCTION inventario.update_longitud_carretera_municipio() RETU
 BEGIN
 
         IF (TG_OP = 'UPDATE') THEN
-           NEW.longitud_tramo = (NEW.pk_final_tramo - NEW.pk_inicial_tramo)*1000;
+           NEW.longitud_tramo = CAST((NEW.pk_final_tramo - NEW.pk_inicial_tramo)*1000 AS integer);
            RETURN NEW;
 
         ELSE IF (TG_OP = 'INSERT') THEN
-           NEW.longitud_tramo = (NEW.pk_final_tramo - NEW.pk_inicial_tramo)*1000;
+           NEW.longitud_tramo = CAST((NEW.pk_final_tramo - NEW.pk_inicial_tramo)*1000 AS integer);
            RETURN NEW;
 
         END IF;
