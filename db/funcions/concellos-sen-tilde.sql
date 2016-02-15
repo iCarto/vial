eafea61f766aa1d1d5f77d2af601c25caea25eb3
Andrés Maneiro <andres.maneiro@gmail.com>
2016-02-15 19:42:52 +0100

    Update concellos table to drop accents

---
+++ db/funcions/concellos-sen-tilde.sql
@@ -0,0 +1,23 @@
+UPDATE inventario.municipio_codigo
+  SET nombre = 'BOVEDA' WHERE nombre = 'BÓVEDA';
+
+UPDATE inventario.municipio_codigo
+  SET nombre = 'GUNTIN' WHERE nombre = 'GUNTÍN';
+
+UPDATE inventario.municipio_codigo
+  SET nombre = 'LANCARA' WHERE nombre = 'LÁNCARA';
+
+UPDATE inventario.municipio_codigo
+  SET nombre = 'LOURENZA' WHERE nombre = 'LOURENZÁ';
+
+UPDATE inventario.municipio_codigo
+  SET nombre = 'PANTON' WHERE nombre = 'PANTÓN';
+
+UPDATE inventario.municipio_codigo
+  SET nombre = 'PORTOMARIN' WHERE nombre = 'PORTOMARÍN';
+
+UPDATE inventario.municipio_codigo
+  SET nombre = 'RABADE' WHERE nombre = 'RÁBADE';
+
+UPDATE inventario.municipio_codigo
+  SET nombre = 'RIBEIRA DE PIQUIN' WHERE nombre = 'RIBEIRA DE PIQUÍN';
